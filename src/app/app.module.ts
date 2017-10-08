02e59e26683f3cf73ec646e06a8f6f89e9666ba9
Sani Yusuf <saniyusuf@gmail.com>
2017-10-08 11:01:17 -0400

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,6 +14,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Http } from "@angular/http";
+
+import "rxjs/add/operator/map";
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  public sitesList$;
+  constructor(public navCtrl: NavController,
+  private http: Http) {
+    this.sitesList$ = this.http
+    .get("https://api.myjson.com/bins/4kwpm")
+    .map(data => data.json());
 
   }
 
